dcace3d2b85940cf72e6b108f0b1a70b49f6439b
Michael Gardner <gardnm4@rpi.edu>
2015-03-16 18:41:07 -0400

    Refreshed names of lab files
    The lab files used to say 'test' from experimenting with the code. Now
    they say 'lab3-1'.

--- lab3-1/lab3-1.c
+++ lab3-1/lab3-1.c
@@ -38,7 +38,7 @@ void main(void)
     //__________________________________________
     PW = PW_CENTER;
     count=0;
-    while (count < 29);
+	while (count < 29);
     PCA0CPL0 = 0xFFFF - PW;
     PCA0CPH0 = (0xFFFF - PW) >> 8;
     while(1)
@@ -65,7 +65,7 @@ void Port_Init()
 //
 void XBR0_Init()
 {
-    
+	
     XBR0  = 0x27;  //configure crossbar as directed in the laboratory
 
 }
@@ -81,10 +81,10 @@ void PCA_Init(void)
     // reference to the sample code in Example 4.5 -Pulse Width Modulation 
     // implemented using the PCA (Programmable Counter Array), p. 50 in Lab Manual.
     PCA0MD = 0x81;
-    PCA0CPM0 = 0xC2;    //CCM0 in 16-bit compare mode
-    PCA0CN = 0x40;      //Enable PCA counter
-    EIE1 |= 0x08;       //Enable PCA interrupt
-    EA = 1;             //Enable global interrupts
+	PCA0CPM0 = 0xC2;	//CCM0 in 16-bit compare mode
+    PCA0CN = 0x40;		//Enable PCA counter
+	EIE1 |= 0x08;		//Enable PCA interrupt
+	EA = 1;				//Enable global interrupts
 }
 
 //-----------------------------------------------------------------------------
@@ -96,14 +96,14 @@ void PCA_Init(void)
 void PCA_ISR ( void ) __interrupt 9
 {
 
-    if (CF)
-    {
-        CF =0;
-        PCA0 = 0x7000;
-        count++;
-    }
+	if (CF)
+	{
+		CF =0;
+		PCA0 = 0x7000;
+		count++;
+	}
 
-    PCA0CN &= 0xC0;
+	PCA0CN &= 0xC0;
     // reference to the sample code in Example 4.5 -Pulse Width Modulation 
     // implemented using the PCA (Programmable Counter Array), p. 50 in Lab Manual.
 }
@@ -118,27 +118,27 @@ void Steering_Servo()
         // ___________________________
         // ___________________________
         if(PW <= PW_MIN)  // check if less than pulsewidth minimum
-        {   
-            PW = PW_MIN;    // set SERVO_PW to a minimum value
-        }
-        else
-        {
-            PW -= 10;
-        }
-    }
+        {	
+			PW = PW_MIN;    // set SERVO_PW to a minimum value
+    	}
+		else
+		{
+			PW -= 10;
+		}
+	}
     else if(input == 'l')  // single character input to decrease the pulsewidth
     {
         // ___________________________
         // ___________________________
         if(PW > PW_MAX)  // check if pulsewidth maximum exceeded
         {
-            PW = PW_MAX;     // set PW to a maximum value
-        }
-        else
-        {
-            PW += 10;
-        }
-    }
+			PW = PW_MAX;     // set PW to a maximum value
+    	}
+		else
+		{
+			PW += 10;
+		}
+	}
     printf("PW: %u\n", PW);
     PCA0CPL0 = 0xFFFF - PW;
     PCA0CPH0 = (0xFFFF - PW) >> 8;
